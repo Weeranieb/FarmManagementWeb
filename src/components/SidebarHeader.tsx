3d7ab8531cfbcfecd8796606438a31781b61dbf4
weeranieb <weerachai.panthueng@gmail.com>
2024-05-18 21:18:57 +0700

    add Icon

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,17 +1,21 @@
 // src/components/SidebarHeader.tsx
 import React from 'react'
 import { Box, Typography } from '@mui/material'
+import SetMealIcon from '@mui/icons-material/SetMeal'
 
 const SidebarHeader: React.FC = () => {
   return (
     <Box sx={{ pt: 2, pb: 3, textAlign: 'center', backgroundColor: '#FAF8EE' }}>
-      <Typography
-        variant='h6'
-        component='div'
-        sx={{ fontFamily: 'Anton, sans-serif', fontSize: '2rem' }}
-      >
-        Boonma Farm
-      </Typography>
+      <Box display='flex' alignItems='center' justifyContent='center'>
+        <SetMealIcon sx={{ mr: 1, fontSize: '2rem' }} />{' '}
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{ fontFamily: 'Anton, sans-serif', fontSize: '2rem' }}
+        >
+          Boonma Farm
+        </Typography>
+      </Box>
     </Box>
   )
 }
