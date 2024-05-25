ac078fdee62f0124a3735dbd3a7f14c1cc8a8ffc
weeranieb <weerachai.panthueng@gmail.com>
2024-05-25 23:15:42 +0700

    add background and loginPage

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,32 +1,76 @@
-import React, { useState } from 'react'
+import React from 'react'
+import {
+  TextField,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Box,
+} from '@mui/material'
 
-const LoginPage: React.FC = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const handleLogin = () => {
-    // Perform login logic here
-    // You can make an API call to authenticate the user
-    // and handle the success or failure accordingly
-  }
-
+const LoginPage = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <input
-        type='text'
-        placeholder='Username'
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type='password'
-        placeholder='Password'
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <Grid
+      container
+      sx={{
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        overflow: 'hidden',
+        '::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url(${process.env.PUBLIC_URL}/images/backgroundLogin.jpg)`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          filter: 'blur(2px)',
+          zIndex: -1,
+        },
+      }}
+    >
+      <Box
+        component='form'
+        sx={{
+          width: '100%',
+          maxWidth: 360,
+          margin: '0 auto',
+          backgroundColor: 'rgba(255, 255, 255, 0.85)',
+          padding: 3,
+          borderRadius: 2,
+          zIndex: 1,
+        }}
+        noValidate
+        onSubmit={(e) => e.preventDefault()}
+      >
+        <TextField
+          sx={{ marginBottom: 2 }}
+          label='Email or phone number'
+          variant='outlined'
+          fullWidth
+          required
+        />
+        <TextField
+          sx={{ marginBottom: 2 }}
+          label='Password'
+          variant='outlined'
+          fullWidth
+          required
+          type='password'
+        />
+        <FormControlLabel
+          control={<Checkbox value='remember' color='primary' />}
+          label='Remember me'
+        />
+        <Button type='submit' variant='contained' color='primary' fullWidth>
+          Sign in
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
