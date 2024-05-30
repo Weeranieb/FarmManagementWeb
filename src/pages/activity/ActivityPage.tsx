0c266811f12bd3fe06e87dfc6f3d0088a3e24001
weeranieb <weerachai.panthueng@gmail.com>
2024-05-30 22:15:04 +0700

    add all necessary elem

--- src/pages/activity/ActivityPage.tsx
+++ src/pages/activity/ActivityPage.tsx
@@ -7,29 +7,71 @@ import {
   InputAdornment,
   Typography,
 } from '@mui/material'
-import { Search as SearchIcon } from '@mui/icons-material'
+import {
+  Search as SearchIcon,
+  Info as InfoIcon,
+  MoreHoriz as MoreHorizIcon,
+} from '@mui/icons-material'
 
 const columns: GridColDef[] = [
-  { field: 'id', headerName: 'NO', width: 70 },
-  { field: 'tank', headerName: 'บ่อ', width: 150 },
+  { field: 'id', headerName: '', width: 70 },
+  { field: 'pond', headerName: 'บ่อ', width: 150 },
   { field: 'activity', headerName: 'กิจกรรม', width: 150 },
   { field: 'farm', headerName: 'ฟาร์ม', width: 150 },
   { field: 'totalWeight', headerName: 'น้ำหนักรวม', width: 150 },
   { field: 'unit', headerName: 'หน่วย', width: 100 },
   { field: 'date', headerName: 'วันที่ทำ', width: 150 },
   { field: 'edit', headerName: 'เพิ่มข้อมูลวันที่', width: 160 },
+  {
+    field: 'actions',
+    headerName: '',
+    width: 100,
+    renderCell: (params) => {
+      if (params.field === 'actions') {
+        return (
+          <Box display='flex' alignItems='center'>
+            <InfoIcon color='disabled' />
+            <MoreHorizIcon color='disabled' />
+          </Box>
+        )
+      }
+      return params.value
+    },
+  },
 ]
 
 const rows = [
   {
     id: 1,
-    tank: 'บ่อ2',
+    pond: 'บ่อ 2',
     activity: 'ย้าย',
     farm: 'บ้านระกาศ',
     totalWeight: '4.3 ต้น',
-    unit: '',
+    unit: 'กิโลกรัม',
     date: '02/08/2566',
     edit: '8/21/15',
+    actions: (
+      <>
+        <InfoIcon color='disabled' />
+        <MoreHorizIcon color='disabled' />
+      </>
+    ), // Add empty string for the info column
+  },
+  {
+    id: 2,
+    pond: 'บ่อ 3',
+    activity: 'ย้าย',
+    farm: 'บ้านระกาศ',
+    totalWeight: '4.3 ต้น',
+    unit: 'กิโลกรัม',
+    date: '02/08/2566',
+    edit: '8/21/15',
+    actions: (
+      <>
+        <InfoIcon color='disabled' />
+        <MoreHorizIcon color='disabled' />
+      </>
+    ), // Add InfoIcon for the info column
   },
 ]
 
@@ -81,13 +123,15 @@ const ExpenseList: React.FC = () => {
           </TextField>
         </Box>
       </Box>
-      <div style={{ height: 400, width: '100%' }}>
+      <div style={{ height: 600, width: '100%' }}>
         <DataGrid
           rows={rows}
           columns={columns}
           sx={{
             '& .MuiDataGrid-columnHeaders': {
               backgroundColor: '#FAF8EE',
+              fontWeight: 'bolder',
+              fontSize: '1.05rem',
             },
             '& .MuiDataGrid-cell': {
               borderBottom: 'none',
