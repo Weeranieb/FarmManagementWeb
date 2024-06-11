dc4ce674fa649f810e48ea4398438cb15296824a
weeranieb <weerachai.panthueng@gmail.com>
2024-06-11 23:23:31 +0700

    add daily feed page

--- src/pages/dailyfeed/DailyFeedPage.tsx
+++ src/pages/dailyfeed/DailyFeedPage.tsx
@@ -16,7 +16,7 @@ import {
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import dayjs, { Dayjs } from 'dayjs'
+import { Dayjs } from 'dayjs'
 import { useDropzone } from 'react-dropzone'
 
 const HomePage: React.FC = () => {
