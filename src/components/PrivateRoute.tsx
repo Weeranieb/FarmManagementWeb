ab78f6236157b7f863a787ab89b2d8bdb159e3ed
weeranieb <weerachai.panthueng@gmail.com>
2024-06-29 09:34:11 +0700

    add comment

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -3,6 +3,7 @@ import { ACCESS_TOKEN_NAME } from '../constants/localStorageConstants'
 
 const PrivateRoute = () => {
   const isAuth = localStorage.getItem(ACCESS_TOKEN_NAME)
+  // TODO check auth via redux store, tricker by accesstoken
   // If authorized, return an outlet that will render child elements
   // If not, return element that will navigate to login page
   return isAuth ? <Outlet /> : <Navigate to='/login' />
